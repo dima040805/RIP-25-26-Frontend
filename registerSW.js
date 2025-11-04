3555687dbf8e9ac9982cceba875b981730898ee8
Muka <dima040805@yandex.ru>
2025-11-04 16:20:19 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/RIP-25-26-Frontend/sw.js', { scope: '/RIP-25-26-Frontend/' })})}
