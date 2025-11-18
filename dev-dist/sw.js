96800fe51a4f07f2e2ce34cd1dfd93ed41521e25
Muka <dima040805@yandex.ru>
2025-11-19 00:55:10 +0300

    gitignore

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "4463db0e243ba5019bc6451d779e754b"
   }, {
     "url": "index.html",
-    "revision": "0.upfv2u6vg6g"
+    "revision": "0.4o2ebuo7g6o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
